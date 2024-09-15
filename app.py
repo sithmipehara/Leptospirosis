bb0d16bd0693e6b4d577bb0e8b8317cbe75251ca
sithmipehara <sithmiwijesinghe20@gmail.com>
2024-09-15 20:29:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,6 @@ p {
 """, unsafe_allow_html=True)
 
 # Display the title using Markdown to allow line breaks 
-st.image("https://www.news.lk/media/k2/items/cache/3cfd4ff673c93395fd580159a9616109_XL.jpg", caption="Sunrise by the mountains")
 st.markdown("<h1 style='text-align: center;color:#FFFFFF;'>Local Leptospirosis Cases<br>(From 2007 - Present)</h1>", unsafe_allow_html=True)
 
 # Custom CSS to change the background color of the sidebar and main area
