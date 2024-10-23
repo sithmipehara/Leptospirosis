25ac7248625aaea5a4ef62cc0d4b650377a94fcc
sithmipehara <sithmiwijesinghe20@gmail.com>
2024-10-23 19:22:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -440,7 +440,7 @@ def prepare_annual_district_data(df):
     return annual_district_data
 
 # Create columns with different widths
-col1, col2, col3 = st.columns(3)  
+col1, col2, col3 = st.columns([1, 2, 1])  
 
 with col1:
     st.markdown("<div class='donut-container'>", unsafe_allow_html=True)
