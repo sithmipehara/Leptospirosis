ed75dde5f92aea15527bdff7bb0b372fae64a896
sithmipehara <sithmiwijesinghe20@gmail.com>
2024-10-23 19:18:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ st.markdown("""
 .stSelectbox {
     transition: background-color 0.3s;
     line-height: 50px;
-    height: 150px; 
+    height: 50px; 
     width:50px;
     text-align: center;
     background-color: #3c3c44; /* Change this to your desired color */
